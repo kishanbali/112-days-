8117d078269e59850d59a624cf26dfd0f5f50c2f
KishanBali <kishansaraiya9@gmail.com>
2026-09-13 23:05:26 +0530

    Add PWA service worker for 112-day tracker

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='kemp-eye-112-pwa-v1';
+const CORE=['./','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,x));return r}).catch(()=>c)))});
