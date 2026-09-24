5a78f4d9168502566585f79252fd25c27e2ca33c
KishanBali <kishansaraiya9@gmail.com>
2026-09-24 23:59:18 +0530

    Refresh PWA cache for Day progression fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kemp-eye-112-pwa-v4';
+const CACHE='kemp-eye-112-pwa-v5';
 const CORE=['./','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
